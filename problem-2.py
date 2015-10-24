7efa8d503d9cafd165a25595ca9c8e74060b4c57
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-10-23 19:47:24 -0700

    p2 done

---
+++ problem-2.py
@@ -0,0 +1,47 @@
+# Problem 2
+# https://projecteuler.net/problem=2
+
+
+def even_fibs():
+
+    summe = 0
+    a,b = 1,1
+
+    while a < 4000000:
+
+        if a % 2 == 0:
+            summe += a
+
+        a,b = b, a + b
+        
+    return summe
+
+
+# The evens_and_odds function is based off the fact that
+# the sum of the evens = the sum of the odds if you end the 
+# summation on an even fibonacci number.
+#
+# Consider the terms up to 8: 1, 1, 2, 3, 5, 8
+# Now, 1 + 1 + 3 + 5 = 10 = 2 + 8
+
+def evens_and_odds():
+
+    summe = 0
+    a,b = 1,1
+
+    while a <= 4000000:
+        summe += a
+        a,b = b, a + b
+
+    return summe / 2
+
+
+if __name__ == "__main__":
+
+    print ("=================================")
+    print ("*          Problem Two          *")
+    print ("*         Andrew Wilson         *")
+    print ("*********************************")
+
+    print (even_fibs())
+    print (evens_and_odds())
