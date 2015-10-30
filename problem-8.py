96801c6f9485d0eb1b5f3b86a482ce71de295c51
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-10-30 11:31:08 -0700

    skeleton of p8

---
+++ problem-8.py
@@ -0,0 +1,13 @@
+# Problem 8
+# https://projecteuler.net/problem=8
+
+import math
+
+
+if __name__ == "__main__":
+
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print ("|           Problem 8           |")
+    print ("|         Andrew Wilson         |")
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
