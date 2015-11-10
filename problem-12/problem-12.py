ff5542a1b0c5f4a068436ee945b78233c78f569d
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-11-09 20:34:00 -0800

    initial commit of p12, not currently working

---
+++ problem-12/problem-12.py
@@ -0,0 +1,27 @@
+# Problem 12
+# https://projecteuler.net/problem=12
+
+import math
+
+
+def find_tri_num():
+
+    with open("primes1.txt") as infile:
+        data = infile.read().strip()
+    print (primes)
+    n, i, factors = 0, 1, 0
+    while factors <= 500:
+        n += i
+        i += 1
+
+
+if __name__ == "__main__":
+
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print ("||          Problem 12           ||")
+    print ("||         Andrew Wilson         ||")
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+    find_tri_num()
+
+
