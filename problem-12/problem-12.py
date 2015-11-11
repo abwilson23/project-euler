83be7779098a9d69971c058cafc52652a8bcf104
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-11-10 20:17:22 -0800

    working solution, very fast

--- problem-12/problem-12.py
+++ problem-12/problem-12.py
@@ -1,18 +1,44 @@
 # Problem 12
 # https://projecteuler.net/problem=12
 
-import math
-
+import time
 
 def find_tri_num():
 
     with open("primes1.txt") as infile:
-        data = infile.read().strip()
-    print (primes)
-    n, i, factors = 0, 1, 0
-    while factors <= 500:
-        n += i
+        primes = [int(x) for x in infile.read().split()]
+
+    i, factors = 1, 0
+    while factors < 500:
+        num = i*(i+1)/2
+        factors = num_of_factors(primes, num)
         i += 1
+
+    print (num)
+
+
+def num_of_factors(primes, num):
+
+    pfactors = []
+    for x in primes:
+        while num % x == 0:
+            pfactors.append(x)
+            num /= x
+        if num == 1:
+            break
+
+    fact_pows = {}
+    for p in pfactors:
+        if p in fact_pows:
+            fact_pows[p] += 1
+        else:
+            fact_pows[p] = 1
+
+    factors = 1
+    for k in fact_pows:
+        factors *= (fact_pows[k] + 1)
+
+    return factors
 
 
 if __name__ == "__main__":
@@ -22,6 +48,8 @@ if __name__ == "__main__":
     print ("||         Andrew Wilson         ||")
     print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
+    start_time = time.time()
     find_tri_num()
+    print ("--- %s seconds ---" % (time.time() - start_time))
 
 
