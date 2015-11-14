ae2618b2068e14ac45f74aed46b68d3112b4dad1
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-11-14 12:40:51 -0800

    working commit of problem-17.py

---
+++ problem-17.py
@@ -0,0 +1,22 @@
+# Problem 17
+# https://projecteuler.net/problem=17
+
+from num2words import num2words
+
+def count_letters():
+
+    letter_count = 0
+    for i in range(1, 1001):
+        letter_count += len(filter(lambda s: s not in ' -', num2words(i)))
+
+    print (letter_count)
+
+
+if __name__ == "__main__":
+
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print ("|          Problem 17           |")
+    print ("|         Andrew Wilson         |")
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    
+    count_letters()
