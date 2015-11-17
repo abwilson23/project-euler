435b819e1ba583c0852b941f8b1090b77a5f79d1
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-11-16 17:58:56 -0800

    initial commit of p30, not finished

---
+++ problem-30.py
@@ -0,0 +1,15 @@
+# Problem 30
+# https://projecteuler.net/problem=30
+
+import time
+
+def digit_powers():
+
+
+if __name__ == "__main__":
+
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print ("|          Problem 30           |")
+    print ("|         Andrew Wilson         |")
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    
