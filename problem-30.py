ffe39b29698ebd784f1e66d5818a86b72e62e9ee
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-11-17 19:49:33 -0800

    working solution, just needed to find an upper bound

--- problem-30.py
+++ problem-30.py
@@ -1,10 +1,13 @@
 # Problem 30
 # https://projecteuler.net/problem=30
 
-import time
-
 def digit_powers():
 
+    digit_sum = 0
+    for i in range(2, 5*(9**5)): 
+        if sum([int(x)**5 for x in str(i)]) == i:
+            digit_sum += i
+    print (digit_sum)
 
 if __name__ == "__main__":
 
@@ -13,3 +16,5 @@ if __name__ == "__main__":
     print ("|         Andrew Wilson         |")
     print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     
+    digit_powers()
+
