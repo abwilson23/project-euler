9e2b79d569e83d7abcab4ccd860a7b1a9fa27890
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-11-18 18:24:27 -0800

    intital commit of p18

---
+++ problem-18.py
@@ -0,0 +1,11 @@
+# Problem 18
+# https://projecteuler.net/problem=18
+
+
+if __name__ == "__main__":
+
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print ("|          Problem 18           |")
+    print ("|         Andrew Wilson         |")
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    
