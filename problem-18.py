4e9d7c074392df847ae350511424cdfaa2a03574
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-11-19 18:17:44 -0800

    Added method stubs and ideas

--- problem-18.py
+++ problem-18.py
@@ -2,6 +2,13 @@
 # https://projecteuler.net/problem=18
 
 
+def create_graph():
+    # Store values in a binary tree
+
+def max_path():
+    # Write a binary search for the max path that guess potential paths
+
+
 if __name__ == "__main__":
 
     print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
