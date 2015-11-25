0766f82da8cecf0b3776ee42ae0964b3c745a0a5
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-11-24 20:27:05 -0800

    intital commit of p19

---
+++ problem-19.py
@@ -0,0 +1,15 @@
+# Problem 19
+# https://projecteuler.net/problem=19
+
+
+def counting_sundays():
+    # Sundays falling on first day of the month between 1901-2000
+
+if __name__ == "__main__":
+
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print ("|          Problem 19           |")
+    print ("|         Andrew Wilson         |")
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    
+    counting_sundays()
