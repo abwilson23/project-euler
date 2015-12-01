77f2a470f060b6ea7f9a126bb70c83df461e20d5
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-11-30 22:34:24 -0800

    working commit of p23

---
+++ problem-23.py
@@ -0,0 +1,18 @@
+# Problem 23
+# https://projecteuler.net/problem=23
+
+
+def non_abundant_sum():
+    # 
+
+def factorsum(n):
+    # return sum of factors of n. Abundant if factor(n) > n
+
+if __name__ == "__main__":
+
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print ("|          Problem 14           |")
+    print ("|         Andrew Wilson         |")
+    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+    
+    non_abundant_sum()
